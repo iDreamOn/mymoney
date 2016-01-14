fc3bf42e7e6662297c0e4e122c645d823d66a722
Berry Semexan <bsemexan@gmail.com>
2016-01-14 18:58:54 -0500

    TG-18

--- spec/models/payment_method_spec.rb
+++ spec/models/payment_method_spec.rb
@@ -2,40 +2,38 @@ require 'rails_helper'
 
 RSpec.describe PaymentMethod, type: :model do
   it 'has a valid factory' do
-    expect(FactoryGirl.build(:debt)).to be_valid
+    expect(FactoryGirl.build(:payment_method)).to be_valid
   end
 
   it 'is invalid without name' do
-    debt = FactoryGirl.build(:debt, name: nil)
-    expect(debt).to_not be_valid
-    expect(debt.errors).to have_key(:name)
+    payment_method = FactoryGirl.build(:payment_method, name: nil)
+    expect(payment_method).to_not be_valid
+    expect(payment_method.errors).to have_key(:name)
   end
 
-  it 'has many spendings' do
-    user = FactoryGirl.create(:user_with_spendings)
-    expect(user.spendings.length).to eq(1)
+  it 'is invalid without description' do
+    payment_method = FactoryGirl.build(:payment_method, description: nil)
+    expect(payment_method).to_not be_valid
+    expect(payment_method.errors).to have_key(:description)
   end
 
   it 'is unique per user' do
     random_id = 323
     user1 = FactoryGirl.build(:user, id: random_id)
-    category1 = FactoryGirl.build(:category, user: user1)
-    account1 = FactoryGirl.build(:account, user: user1)
-    debt_1 = FactoryGirl.create(:debt, category: category1, account: account1)
-    debt_2 = FactoryGirl.build(:debt, category: category1, account: account1, name: debt_1.name)
+    description1 = FactoryGirl.build(:description, user: user1)
+    pay1 = FactoryGirl.create(:payment_method, description: description1)
+    pay2 = FactoryGirl.build(:payment_method, description: description1, name: pay1.name)
+    expect(pay2).to_not be_valid
+  end
 
-    expect(debt_2).to_not be_valid
+  it 'has many spendings' do
+    payment_method = FactoryGirl.create(:user_with_spendings)
+    expect(payment_method.spendings.length).to eq(1)
   end
 
   it 'must belong to user' do
-    debt = FactoryGirl.build(:debt, name: nil)
-    expect(debt).to_not be_valid
-    expect(debt.errors).to have_key(:name)
-  end
-
-  it 'is invalid without description' do
-    debt = FactoryGirl.build(:debt, name: nil)
-    expect(debt).to_not be_valid
-    expect(debt.errors).to have_key(:name)
+    payment_method = FactoryGirl.build(:payment_method, name: nil)
+    expect(payment_method).to_not be_valid
+    expect(payment_method.errors).to have_key(:name)
   end
 end
