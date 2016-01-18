cb00a0aa773d569735e660ed91ba7fedd5d93540
Sebastien Thomas <titbabthomas@gmail.com>
2016-01-18 07:52:25 -0500

    testing graphs

--- spec/controllers/spendings_controller_spec.rb
+++ spec/controllers/spendings_controller_spec.rb
@@ -167,4 +167,31 @@ RSpec.describe SpendingsController, type: :controller do
       expect(response).to redirect_to(spendings_url)
     end
   end
+
+  describe 'GET #graphs' do
+    before(:each) do
+      spending = Spending.create! valid_attributes
+      login(spending.owner)
+    end
+
+    it 'renders a json for spendings_by_month' do
+      get :spendings_by_month, {}, valid_session
+      expect(response.header['Content-Type']).to match(/json/)
+    end
+
+    it 'renders a json for spendings_by_category' do
+      get :spendings_by_category, {}, valid_session
+      expect(response.header['Content-Type']).to match(/json/)
+    end
+
+    it 'renders a json for spendings_by_payment_method' do
+      get :spendings_by_payment_method, {}, valid_session
+      expect(response.header['Content-Type']).to match(/json/)
+    end
+
+    it 'renders a json for cc_purchase_vs_payment' do
+      get :cc_purchase_vs_payment, {}, valid_session
+      expect(response.header['Content-Type']).to match(/json/)
+    end
+  end
 end
