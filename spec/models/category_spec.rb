302715a1c7448ad4753d729528572a87746c115a
fabrice <defay.fabrice@gmail.com>
2016-01-18 19:02:22 -0500

    adds more changes to category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,25 +5,22 @@ RSpec.describe Category, type: :model do
     expect(FactoryGirl.build(:category)).to be_valid
   end
 
-  it 'is invalid without name' do
+  it 'must have a name' do
     category = FactoryGirl.build(:category, name: nil)
     expect(category).to_not be_valid
     expect(category.errors).to have_key(:name)
   end
 
-  it 'is invalid without description' do
+  it 'must have a description' do
     category = FactoryGirl.build(:category, description: nil)
     expect(category).to_not be_valid
     expect(category.errors).to have_key(:description)
   end
 
-  it 'is unique per user' do
-    random_id = 323
-    user1 = FactoryGirl.build(:user, id: random_id)
-    description1 = FactoryGirl.build(:description, user: user1)
-    pay1 = FactoryGirl.create(:category, description: description1)
-    pay2 = FactoryGirl.build(:category, description: description1, name: pay1.name)
-    expect(pay2).to_not be_valid
+  it 'must be unique per user' do
+    cat1 = FactoryGirl.create(:category)
+    cat2 = FactoryGirl.build(:category, user: cat1.user)
+    expect(cat2).to_not be_valid
   end
 
   it 'must belong to user' do
@@ -32,18 +29,16 @@ RSpec.describe Category, type: :model do
     expect(category.errors).to have_key(:user)
   end
 
-  it 'has many spendings' do
-    category = FactoryGirl.create(:user_with_spendings)
-    expect(category.spendings.length).to eq(1)
+  it 'can has many debts' do
+    category = FactoryGirl.create(:category_with_debts)
+    expect(category.debts.length).to eq(2)
+  end
+
+  it 'can has many budgets' do
+    category = FactoryGirl.create(:category_with_budgets)
+    expect(category.budgets.length).to eq(2)
   end
 
 end
 
 
-
-#spending Ex: must belong to
-	it 'budget is empty' do
-      spending = FactoryGirl.build(:spending, budget: nil)
-      expect(spending).to_not be_valid
-      expect(spending.errors).to have_key(:budget)
-    end 
