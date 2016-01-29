7230519b39bb3221ddbdaa52505b981de2c2abc3
Berry Semexan <bsemexan@gmail.com>
2016-01-28 22:35:03 -0500

    TG-22

--- spec/models/budget_spec.rb
+++ spec/models/budget_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Budget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:budget)).to be_valid
+  end
+
+  it 'is invalid without category' do
+    budget = FactoryGirl.build(:debt, category: nil)
+    expect(budget).to_not be_valid
+    expect(budget.errors).to have_key(:category)
+  end
+
+  it 'is invalid without an amount' do
+    budget = FactoryGirl.build(:budget, amount: nil)
+    expect(budget).to_not be_valid
+    expect(budget.errors).to have_key(:amount)
+  end
+  it 'is invalid without budget month' do
+    budget = FactoryGirl.build(:budget, budget_month: nil)
+    expect(budget).to_not be_valid
+    expect(budget.errors).to have_key(:budget_month)
+  end
+
+  it 'has many spendings' do
+    budget = FactoryGirl.create(:user_with_spendings)
+    expect(budget.spendings.length).to eq(1)
+  end
+
+  it 'is unique per budget' do
+    budget1 = FactoryGirl.create(:budget)
+    budget2 = FactoryGirl.build(:budget, category: budget1.category, budget_month: budget1.budget_month)
+    expect(budget2).to_not be_valid
+  end
 end
