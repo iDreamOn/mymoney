1c93db0d57209d0f40a6a8717af29b38a8da386c
Berry Semexan <bsemexan@gmail.com>
2016-02-11 00:23:17 -0500

    TG-94

--- spec/requests/payment_methods_spec.rb
+++ spec/requests/payment_methods_spec.rb
@@ -1,9 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe 'PaymentMethods', type: :request do
-  describe 'GET /payment_methods' do
-    it 'works! (now write some real specs)' do
-      skip 'TO BE IMPLEMENTED'
+  let(:payment_method) do
+    create(:payment_method)
+  end
+  context 'respond with 200 (SUCCESS) when logged in' do
+    before(:each) do
+      login(payment_method.owner)
+    end
+    it 'payment_methods_path' do
+      get payment_methods_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'new_payment_method_path' do
+      get new_payment_method_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'edit_payment_method_path' do
+      get edit_payment_method_path(payment_method)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'payment_method_path' do
+      get payment_method_path(payment_method)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'spendings_by_payment_method_spendings_path' do
+      get spendings_by_payment_method_spendings_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'respond with 302 (FOUND) when NOT logged in' do
+    it 'payment_methods_path' do
+      get payment_methods_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'new_payment_method_path' do
+      get new_payment_method_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'edit_payment_method_path' do
+      get edit_payment_method_path(payment_method)
+      expect(response).to have_http_status(302)
+    end
+
+    it 'payment_method_path' do
+      get payment_method_path(payment_method)
+      expect(response).to have_http_status(302)
+    end
+
+    it 'spendings_by_payment_method_spendings_path' do
+      get spendings_by_payment_method_spendings_path
+      expect(response).to have_http_status(302)
     end
   end
 end
