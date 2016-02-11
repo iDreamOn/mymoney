67ee77c807106005f139bdcae3c7e527e625de60
Alain P <kazou83@yahoo.com>
2016-02-11 13:02:56 +0000

    TG-92 add layout for debt requests

--- spec/requests/debts_spec.rb
+++ spec/requests/debts_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Debts', type: :request do
-  describe 'GET /debts' do
-    it 'works! (now write some real specs)' do
-      skip 'TO BE IMPLEMENTED'
+  let(:debt) do
+    create(:debt)
+  end
+
+  context 'respond with 200 (SUCCESS) when logged in' do
+    before(:each) do
+      login(debt.owner)
     end
+
+    it 'debts_path' do
+      get debts_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'new_debt_path'
+
+    it 'edit_debt_path'
+
+    it 'debt_path'
+  end
+
+  context 'respond with 302 (FOUND) when NOT logged in' do
+    it 'debts_path' do
+      get debts_path
+      expect(response).to have_http_status(302)
+    end
+    it 'new_debt_path'
+
+    it 'edit_debt_path'
+
+    it 'debt_path'
   end
 end
