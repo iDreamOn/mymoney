b010c042001d3ce81a81ae9cd129259d58741fdc
Berry Semexan <bsemexan@gmail.com>
2016-02-12 19:27:13 -0500

    TG-88

--- spec/requests/account_balances_spec.rb
+++ spec/requests/account_balances_spec.rb
@@ -7,3 +7,37 @@ RSpec.describe 'AccountBalances', type: :request do
     end
   end
 end
+it 'make_payments_account_balance_path' do
+  get make_payments_account_balance_path
+  expect(response).to have_http_status(200)
+end
+
+it 'undo_payments_account_balance_path' do
+  get undo_payments_account_balance_path
+  expect(response).to have_http_status(200)
+end
+
+it 'balances_by_day_account_balances_path' do
+  get balances_by_day_account_balances_path
+  expect(response).to have_http_status(200)
+end
+
+it 'account_balances_path' do
+  get account_balances_path
+  expect(response).to have_http_status(200)
+end
+
+it 'new_account_balance_path' do
+  get new_account_balance_path
+  expect(response).to have_http_status(200)
+end
+
+it 'edit_account_balance_path' do
+  get edit_account_balance_path(account_balance)
+  expect(response).to have_http_status(200)
+end
+
+it 'account_balance_path' do
+  get account_balance_path(account_balance)
+  expect(response).to have_http_status(200)
+end
--- spec/requests/budgets_spec.rb
+++ spec/requests/budgets_spec.rb
@@ -1,9 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe 'Budgets', type: :request do
-  describe 'GET /budgets' do
-    it 'works! (now write some real specs)' do
-      skip 'TO BE IMPLEMENTED'
+  let(:budget) do
+    create(:budget)
+  end
+
+  context 'respond with 200 (SUCCESS) when logged in' do
+    before(:each) do
+      login(budget.owner)
+    end
+
+    it 'reset_budgets_path' do
+      get reset_budgets_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'reset_current_month_budgets_path' do
+      get reset_current_month_budgets_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'budgets_by_month_budgets_path' do
+      get budgets_by_month_budgets_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'budgets_path' do
+      get budgets_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'new_budget_path' do
+      get new_budget_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'edit_budget_path' do
+      get edit_budget_path(budget)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'budget_path' do
+      get budget_path(budget)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context 'respond with 302 (FOUND) when NOT logged in' do
+    it 'reset_budgets_path' do
+      get reset_budgets_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'reset_current_month_budgets_path' do
+      get reset_current_month_budgets_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'budgets_by_month_budgets_path' do
+      get budgets_by_month_budgets_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'budgets_path' do
+      get budgets_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'new_budget_path' do
+      get new_budget_path
+      expect(response).to have_http_status(302)
+    end
+
+    it 'edit_budget_path' do
+      get edit_budget_path(budget)
+      expect(response).to have_http_status(302)
+    end
+
+    it 'budget_path' do
+      get budget_path(budget)
+      expect(response).to have_http_status(302)
     end
   end
 end
