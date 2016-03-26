91ec028b00499c1f9d64e7ea3dbb2958694ac012
Sebastien Thomas <titbabthomas@gmail.com>
2016-03-26 03:59:53 -0400

    Update debt_balances_controller.rb

--- app/controllers/debt_balances_controller.rb
+++ app/controllers/debt_balances_controller.rb
@@ -25,7 +25,7 @@ class DebtBalancesController < ApplicationController
     h1 = {}
 
     last_n_months(12).reverse_each do |date|
-      end_date = [date[1].end_of_month, Spending.maximum(:spending_date) || Time.now.to_date].min
+      end_date = [date[1].end_of_month, Time.now.to_date].min
       total_debt = 0
       current_user.get_all('debt_balances').joins(debt: :category).where("debt_balances.payment_start_date <= '#{end_date}' AND '#{end_date}' <= due_date AND debts.is_asset=false AND categories.name NOT IN ('Rent','Donation/Gift (Recurring)')").each { |k| total_debt += k.max_payment(end_date, true) }
       h1.store(date[0], total_debt)
