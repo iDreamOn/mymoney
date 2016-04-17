b74825e4417eb0e3e2634878f89c55f13a669413
Sebastien Thomas <titbabthomas@gmail.com>
2016-04-17 17:08:26 -0400

    fix factory. allow empty account

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -6,7 +6,7 @@ class PaymentMethod < ActiveRecord::Base
 
   validates_presence_of :description, :name, :user, :method_type
   validates_uniqueness_of :name, case_sensitive: false, scope: :user
-  validates_uniqueness_of :account, scope: :user
+  validates_uniqueness_of :account, scope: :user, allow_blank: true
 
   def to_s
     name
