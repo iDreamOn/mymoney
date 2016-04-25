f62dc9080ab3df9590645c4cf7c88095a92966a0
Sebastien Thomas <titbabthomas@gmail.com>
2016-04-25 01:55:47 -0400

    fix tests. allow admin to update others role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def authorize(object = nil)
     owner = object.owner || User.new
-    return if owner == current_user || (owner.contributors.include?(current_user) && current_user.contributors.include?(owner))
+    return if current_user.admin? || owner == current_user || (owner.contributors.include?(current_user) && current_user.contributors.include?(owner))
     flash[:error] = 'You dont have permission to access this section'
     redirect_to root_url
   end
@@ -28,6 +28,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :username, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :username, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :username, :password, :password_confirmation, :current_password, :role) }
   end
 end
